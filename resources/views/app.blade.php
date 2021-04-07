87bddfa507ae3546e878f8410cf0bda62e7ef60f
Richard Bunker <richardjamesbunker@gmail.com>
2021-04-07 10:24:06 +1000

    First commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+   <meta charset="UTF-8" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <script src="{{ mix('/js/app.js') }}" defer></script>
+    <title>Laravel React Inertiajs</title>
+  </head>
+  <body>
+    @inertia
+  </body>
+</html>
